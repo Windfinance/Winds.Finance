86c255dd3f945306d6ba99b2ee41a269d857fcf6
z.buszyk@softflix.de <z.buszyk@softflix.de>
2021-05-09 17:35:05 +0200

    qwewqewqe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
-console.log('Using api endpoint: ', process.env.API_URL);
 
 module.exports = withPlugins([withImages], {
 	env: {
