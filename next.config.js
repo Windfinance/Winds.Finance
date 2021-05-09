511e080e8524e333d96f300f5fbffccdfa621fe6
z.buszyk@softflix.de <z.buszyk@softflix.de>
2021-05-09 16:39:17 +0200

    qweqweweq

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withImages = require('next-images');
 console.log('Using api endpoint: ', process.env.API_URL);
 
 module.exports = withPlugins([withImages], {
-	distDir: 'build',
 	env: {
 		NEXT_PUBLIC_API_URL: process.env.API_URL
 	}
