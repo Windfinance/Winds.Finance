9048712f86c88d29beac17fbfd44face00bef08b
z.buszyk@softflix.de <z.buszyk@softflix.de>
2021-05-10 02:21:05 +0200

    responsive

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,15 +2,27 @@ import styles from './Navigation.module.scss';
 import Link from 'next/link';
 import Tooltip from '../Tooltip';
 import SmoothScroll from 'shared/functions/SmoothScroll';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
+import { useWindowWidth } from '@react-hook/window-size';
 
 function Navigation() {
 	const Scroll = (section: string) => {
 		SmoothScroll.scrollTo(section);
 	};
 
+	const [isOpen, setOpen] = useState(false);
+	const windowWidth = useWindowWidth();
+
 	return (
 		<div className={styles.main}>
 			<img className={styles['main__logo']} src={require('assets/images/x.svg')} alt='avatar' />
+
+			{windowWidth <= 956 && (
+				<div className={styles.hamburger}>
+					<Hamburger toggled={isOpen} toggle={setOpen} />
+				</div>
+			)}
 
 			<ul className={styles['main__navigation']}>
 				<li>
@@ -35,6 +47,35 @@ function Navigation() {
 					<Link href={'#'}>Trade</Link>{' '}
 				</li>
 			</ul>
+
+			{isOpen && (
+				<ul className={styles['navigation-mobile']}>
+					<li>
+						<img className={styles['main__logo']} src={require('assets/images/x.svg')} alt='avatar' />
+					</li>
+					<li>
+						<a onClick={() => Scroll('About')}>About</a>
+					</li>
+
+					<li>
+						<a onClick={() => Scroll('Tokenomics')}>Tokenomics</a>
+					</li>
+
+					<li>
+						<a onClick={() => Scroll('Roadmap')}>Roadmap</a>
+					</li>
+
+					<li>
+						<Tooltip title={'Comming Soon'}>
+							<a>Litepaper</a>
+						</Tooltip>
+					</li>
+
+					<li className={styles.trade}>
+						<Link href={'#'}>Buy Token</Link>{' '}
+					</li>
+				</ul>
+			)}
 		</div>
 	);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp() {
 	return (
 		<>
 			<Head>
-				<title>Wind.Finance</title>
+				<title>Winds.Finance</title>
 				<link rel='icon' href='/favicon.ico' />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
 			</Head>
